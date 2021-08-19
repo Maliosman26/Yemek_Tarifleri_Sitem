ec7c8077c7a09a9af118a8de6daed8ceeb8a6b8e
Maliosman26 <muhammetaliosmanozyurt@gmail.com>
2021-08-19 10:59:50 +0300

    Taking categories with datalist

--- Yemek_Tarifleri_Sitem/Kullanici.Master.cs
+++ Yemek_Tarifleri_Sitem/Kullanici.Master.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace Yemek_Tarifleri_Sitem
 {
     public partial class Site1 : System.Web.UI.MasterPage
     {
+        sqlsinif bgl = new sqlsinif(); //bgl = bağlam
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            SqlCommand komut = new SqlCommand("Select * From Tbl_Kategoriler", bgl.baglanti());
+            SqlDataReader oku = komut.ExecuteReader();
+            DataList1.DataSource = oku;
+            DataList1.DataBind();
         }
     }
 }
